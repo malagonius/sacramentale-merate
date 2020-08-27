63f800ffd3464eaea0a1bf8b773fd505c6791874
malagonius <47065208+malagonius@users.noreply.github.com>
2020-08-27 17:50:50 +0200

    Update index.php

--- Partecipazioni Chiesa/index.php
+++ Partecipazioni Chiesa/index.php
@@ -1,6 +1,9 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css.css">
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> 
+
+
 <div class="container">
 	<div  class="container img_container">
 		<div class="logo_span"></div>
