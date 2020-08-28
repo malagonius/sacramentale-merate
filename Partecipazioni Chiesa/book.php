066935d58c24f685cb0fce166441481334bcce63
malagonius <47065208+malagonius@users.noreply.github.com>
2020-08-28 11:34:27 +0200

    Add files via upload

---
+++ Partecipazioni Chiesa/book.php
@@ -0,0 +1,31 @@
+    <?php
+    header('Content-Type: application/json');
+
+    $aResult = array();
+
+    if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
+
+    if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
+
+    if( !isset($aResult['error']) ) {
+
+        switch($_POST['functionname']) {
+            case 'book':
+               if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+                   $aResult['error'] = 'Error in arguments!';
+               }
+               else {
+                   $aResult['result'] =  $myfile = file_put_contents('logs.txt', $_POST['arguments'] , FILE_APPEND | LOCK_EX);
+               }
+               break;
+
+            default:
+               $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+               break;
+        }
+
+    }
+
+    echo json_encode($aResult);
+
+?>
