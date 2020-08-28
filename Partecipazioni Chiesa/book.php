e62573964a0b45205c98dca80a751d529fabe651
malagonius <47065208+malagonius@users.noreply.github.com>
2020-08-28 12:35:23 +0200

    Add files via upload

--- Partecipazioni Chiesa/book.php
+++ Partecipazioni Chiesa/book.php
@@ -1,17 +1,6 @@
     <?php
     header('Content-Type: application/json');
 
-    
-
-
-
-
-
-
-
-
-
-/*
     $aResult = array();
 
     if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
@@ -22,10 +11,7 @@
 
         switch($_POST['functionname']) {
             case 'book':
-                 //$v = json_decode(stripslashes(file_get_contents("php://input")));
-                 //$inputJSON = $_POST['arguments'];
-                 $input= json_decode( $_POST['arguments'], true ); 
-                 $aResult['result'] =  $myfile = file_put_contents('data.txt', $input , FILE_APPEND | LOCK_EX);
+                 $aResult['result'] =  $myfile = file_put_contents('data.txt', $_POST['arguments'] , FILE_APPEND | LOCK_EX);
                }
                break;
 
@@ -35,7 +21,7 @@
         }
 
     }
-*/
+
     echo json_encode($_POST['arguments']);
 
 ?>
