d4bfe0aa26be1d2acf0c66a41036e7535c3c0955
malagonius <47065208+malagonius@users.noreply.github.com>
2020-08-29 12:14:52 +0200

    Add files via upload

--- Partecipazioni Chiesa/book.php
+++ Partecipazioni Chiesa/book.php
@@ -12,11 +12,12 @@
         switch($_POST['functionname']) {
 
             case 'book': 
+
                   $aResult['result'] =  $myfile = file_put_contents('data.txt', $_POST['arguments'].PHP_EOL, FILE_APPEND | LOCK_EX);
                break;
 
             case 'load':
-                  $aResult['result'] = file_get_contents('data.txt');
+                  $aResult['result'] = file_get_contents('            https://felix-cloud-shared-1-sacramentale-merate-335746352675.s3-accesspoint.us-west-1.amazonaws.com/sacramentale-merate/data.txt?response-content-disposition=attachment%3B%20filename%3D%22data.txt%22%3B%20filename%2A%3DUTF-8%27%27data.txt&response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAU4LAKYYRWQKUA2OB%2F20200829%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Date=20200829T101314Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=e4c66778f5c3d4239e2dface0da663434a916d6203874adf0168686cfac54b4c');
                 break;
                 
             case 'delete-all':
