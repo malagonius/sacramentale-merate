8259f8c52ee5951a4e958794b8534ae66d3308d2
malagonius <47065208+malagonius@users.noreply.github.com>
2020-09-10 11:15:30 +0200

    Update js.js

--- Partecipazioni Chiesa/js.js
+++ Partecipazioni Chiesa/js.js
@@ -263,3 +263,31 @@ validInputs = function(nome,quantita){
 	}
 	return ret;
 }
+
+
+deleteRecords = function(){
+	var uploadURL ="https://api.github.com/repos/malagonius/sacramentale-merate/contents/data.txt";
+	var newData = "\n";
+	$.ajax({
+		type: "PUT",
+		url: uploadURL,
+		contentType: "application/json",
+		dataType: "json",
+		headers: {
+			    "accept": "application/vnd.github.v3+json",
+			    "Authorization": "Basic bWFsYWdvbml1czo0NjJhMjZjZjA3ZTMxMTU5NzkyMzFmNjkzNjIxOTk4NzdmYmQ3ODAx",
+			    "Content-Type": "application/json",
+			},
+		data: JSON.stringify({
+		"message": "records were cleared",
+		"content": btoa(newData),
+		"sha": loadedData.sha
+	    }),
+
+	})
+	  .done(function( msg ) {
+	    console.log( "Data Saved: " + json );
+	    loadData();
+	  });
+	
+}
