1c427168a9b2ad5db53e7d3744ea4b74b549bb3c
malagonius <47065208+malagonius@users.noreply.github.com>
2022-02-06 18:39:36 +0100

    Update ccCleanerOP.php

--- ccCleanerOP.php
+++ ccCleanerOP.php
@@ -1,36 +1,12 @@
 <html>
 	<head>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
+	        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="js.js"></script>
+        </head>
 <script>
-deleteRecords = function(){
-	var uploadURL ="https://api.github.com/repos/malagonius/sacramentale-merate/contents/data.txt";
-	var newData = "\n";
-	$.ajax({
-		type: "PUT",
-		url: uploadURL,
-		contentType: "application/json",
-		dataType: "json",
-		headers: {
-			    "accept": "application/vnd.github.v3+json",
-			    "Authorization": "Basic bWFsYWdvbml1czo0NjJhMjZjZjA3ZTMxMTU5NzkyMzFmNjkzNjIxOTk4NzdmYmQ3ODAx",
-			    "Content-Type": "application/json",
-			},
-		data: JSON.stringify({
-		"message": "records were cleared",
-		"content": btoa(newData),
-		"sha": loadedData.sha
-	    }),
-
-	})
-	  .done(function( msg ) {
-	    console.log( "Data Saved: " + json );
-	    loadData();
-	  });
-	
-}
 
 window.onload = function(){
+	loadData();
 	deleteRecords();
 }
 
